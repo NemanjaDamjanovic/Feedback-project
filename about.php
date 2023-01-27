56928da348da239908f0e8a0aba1f69e56b66f98
Nemanja Damjanovic <95293288+NemanjaDamjanovic@users.noreply.github.com>
2023-01-27 17:57:29 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php'; ?>
+
+    <h2>About</h2>
+
+    <p class="text-center">Ranch Harmonija is located in western Serbia near city of Valjevo. Its surrounded by beautiful nature. Less then a 1 km is located canyon of the river Gradac, pearl of nature, spot where horses from our ranch really like to spend their time. If you didnt we suggest you to visit us and also share your experience in this feedback. Hope we will see you soon and thanks in advance on your time!</p>
+    
+
+<?php include 'includes/footer.php'; ?>
