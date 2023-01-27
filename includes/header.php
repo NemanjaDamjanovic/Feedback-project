ce68d027f1b242a897afa29bd452559f512bb831
Nemanja Damjanovic <95293288+NemanjaDamjanovic@users.noreply.github.com>
2023-01-27 18:05:20 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,47 @@
+<?php include './configuration/database.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Leave Feedback</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+    <div class="container">
+      <a class="navbar-brand" href="index.php" style="color: saddlebrown">Ranch Harmonija</a>
+      
+        <div>
+        <a href="https://www.instagram.com/ranc.harmonija/?hl=en">
+        <img src="images/insta.png" style="width:25px; height:25px;" alt="Example Image">
+        </a>
+        </div>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+         <li class="nav-item">
+              <a class="nav-link" href="index.php" style="color: saddlebrown">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="feedback.php" style="color: saddlebrown"
+                >Feedback</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php" style="color: saddlebrown"
+                >About</a
+              >
+            </li>
+        </ul>
+      </div>
+  </div>
+</nav>
+
+<main>
+  <div class="container d-flex flex-column align-items-center">
